ad5e357d23df32f5bec36f123436286cc52eaf13
gearnode <bryan.frimin@exograd.com>
2021-12-24 12:51:42 +0100

    Add context type def

---
+++ src/context.ts
@@ -0,0 +1,21 @@
+import type { Id } from "./index.js";
+
+import { readFile } from 'fs';
+
+export interface Context {
+    readonly event: Event;
+    readonly task_parameters: Object;
+    readonly instance_id: Id;
+    readonly identities: Object;
+}
+
+export interface Event {
+    readonly id: Id;
+    readonly org_id: Id;
+    readonly trigger_id?: Id;
+    readonly command_id?: Id;
+    readonly creation_time: Date;
+    readonly event_time: Date;
+    readonly name: string;
+    readonly data: Object;
+}
