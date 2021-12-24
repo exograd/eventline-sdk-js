c57b5e562926d365a9eb06e28c375fdf15d9eb83
gearnode <bryan.frimin@exograd.com>
2021-12-24 13:11:09 +0100

    Add function to get current project id

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 export type Id = string;
+export type MaybeId = Id | undefined;
 
 export function isExecutedInEventline(): boolean {
     return process.env.EVENTLINE === "true";
+}
+
+export function getCurrentProjectId(): MaybeId {
+    return process.env.EVENTLINE_PROJECT_ID;
 }
 
