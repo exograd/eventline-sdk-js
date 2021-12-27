7b36a4aba000578f746dbed8942566d49a9ec8fe
gearnode <bryan.frimin@exograd.com>
2021-12-27 13:21:29 +0100

    Add command type

---
+++ src/command.ts
@@ -0,0 +1,16 @@
+export interface Command {
+    parameters: CommandParameters;
+    pipelines: PipelineName[];
+}
+
+export type PipelineName = string;
+
+export type ParameterType = "string" | "number" | "boolean";
+
+export interface CommandParameters {
+    name: string;
+    description: string;
+    type: ParameterType;
+    default: any;
+    environment: string;
+}
