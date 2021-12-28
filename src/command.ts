264157e88a541b0b2f82ed8234e6d439e2edde17
gearnode <bryan.frimin@exograd.com>
2021-12-28 12:18:08 +0100

    Add getCommandByName

--- src/command.ts
+++ src/command.ts
@@ -1,16 +1,38 @@
+import type { Id } from "@ev";
+import type { Client } from "@ev/client";
+import type { Resource } from "@ev/resource";
+
+import { makeClient } from "@ev/client";
+
 export interface Command {
-  parameters: CommandParameters;
-  pipelines: PipelineName[];
+  id: Id;
+  org_id: Id;
+  project_id: Id;
+  creation_time: string;
+  update_time: string;
+  disabled: boolean;
+
+  spec: Resource<CommandData>;
 }
 
-export type PipelineName = string;
+export interface CommandData {
+  parameters: CommandParameter[];
+  pipelines: string[];
+}
 
-export type ParameterType = "string" | "number" | "boolean";
-
-export interface CommandParameters {
-  name: string;
-  description?: string;
-  type: ParameterType;
-  default?: any;
+export interface CommandParameter {
+  default?: string;
   environment?: string;
+  name: string;
+  values?: string[];
+  description?: string;
+  type: "string" | "number" | "boolean";
+}
+
+export async function getCommandByName(
+  client: Client,
+  name: string
+): Promise<Command> {
+  const command: Command = await client("GET", "/v0/commands/name/" + name);
+  return command;
 }
