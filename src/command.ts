e61098048ffe8d71cc60e8c95a6c27dd8cf445b1
gearnode <bryan.frimin@exograd.com>
2021-12-29 14:24:14 +0100

    Add executeCommand

--- src/command.ts
+++ src/command.ts
@@ -43,3 +43,21 @@ export async function getCommand(
 ): Promise<GetCommandResponse> {
   return getResource(client, request);
 }
+
+export interface ExecuteCommandRequest {
+  id: string;
+  parameters: Record<string, string | number | boolean>;
+}
+
+export interface ExecuteCommandResponse {
+  pipeline_ids: string[];
+}
+
+export async function executeCommand(
+  client: Client,
+  request: ExecuteCommandRequest
+): Promise<ExecuteCommandRequest> {
+  const data = JSON.stringify(request.parameters);
+  const url = "/v0/commands/id/" + request.id + "/execute";
+  return client("POST", url, data);
+}
