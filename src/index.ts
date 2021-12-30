c301c74fafca5643361fa10bc458de003e002c2e
gearnode <bryan.frimin@exograd.com>
2021-12-30 10:07:15 +0100

    Add doc for the index functions

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,41 @@
 export type Id = string;
 export type Maybe<T> = T | undefined;
 
+/**
+ * Returns `true` when the function is called in an Eventline instance.
+ */
 export function isExecutedInEventline(): boolean {
   return process.env.EVENTLINE === "true";
 }
 
+/**
+  * Returns the current project id when the function is called in an
+  * Eventline instance.
+ */
 export function getCurrentProjectId(): Maybe<Id> {
   return process.env.EVENTLINE_PROJECT_ID;
 }
 
+/**
+ * Returns the current project name when the function is called in an
+ * Eventline instance.
+ */
 export function getCurrentProjectName(): Maybe<string> {
   return process.env.EVENTLINE_PROJECT_NAME;
 }
 
+/**
+ * Returns the current pipeline id when the function is called in an
+ * Eventline instance.
+ */
 export function getCurrentPipelineId(): Maybe<Id> {
   return process.env.EVENTLINE_PIPELINE_ID;
 }
 
+/**
+ * Returns the current task id when the function is called in an
+ * Eventline instance.
+ */
 export function getCurrentTaskId(): Maybe<Id> {
   return process.env.EVENTLINE_TASK_ID;
 }
