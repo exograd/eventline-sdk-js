10be6fe2363d7936ba5c62d3533de816e207a72d
gearnode <bryan.frimin@exograd.com>
2021-12-30 10:36:04 +0100

    Add function to list and get events

--- src/event.ts
+++ src/event.ts
@@ -1,4 +1,7 @@
 import type { Id } from "@ev";
+import type { Client, ListResponse } from "@ev/client";
+
+import url from "url";
 
 export interface Event {
   id: Id;
@@ -9,4 +12,40 @@ export interface Event {
   event_time: string;
   name: string;
   data: Object;
+}
+
+export interface ListEventsRequest {
+  after?: Id;
+  size?: number;
+  pipelineId?: Id;
+  connector?: string;
+  name?: string;
+}
+
+export type ListEventsResponse = ListResponse<Event>;
+
+export async function listEvents(
+  client: Client,
+  request: ListEventsRequest
+): Promise<ListEventsResponse> {
+  const query: Record<string, string> = {};
+
+  if (request.after !== undefined) query.after = request.after;
+  if (request.size !== undefined) query.size = request.size.toString();
+  if (request.pipelineId !== undefined) query.pipeline_id = request.pipelineId;
+
+  return client("GET", url.format({ pathname: "/v0/events", query: query }));
+}
+
+export interface GetEventRequest {
+  id: string;
+}
+
+export type GetEventResponse = Event;
+
+export async function getEvent(
+  client: Client,
+  request: GetEventRequest
+): Promise<GetEventResponse> {
+  return client("GET", "/v0/events/id/" + request.id);
 }
