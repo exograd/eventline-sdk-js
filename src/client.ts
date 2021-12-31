ccd4a4dcd066ba7fcbf2e9b15dfe89c200c15799
gearnode <bryan.frimin@exograd.com>
2021-12-31 15:23:40 +0100

    Replace certificate pinning by public key pinning
    Certificate pinning is hard to maintain as it's impossible to update
    the certificate without breaking existing library releases.  Public key
    pinning allows updating the certificate without breaking anything and
    offers the same security guarantee.

--- src/client.ts
+++ src/client.ts
@@ -14,11 +14,14 @@
 // TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
-import type { TLSSocket } from "tls";
+import type { PeerCertificate } from "tls";
 import type { Id } from "@ev";
 
-import { request as httpRequest } from "https";
-import { readFileSync } from "fs";
+import https from "https";
+import fs from "fs";
+import url from "url";
+import tls from "tls";
+import crypto from "crypto";
 
 export type Verb = "GET" | "POST" | "PUT" | "DELETE";
 
@@ -57,131 +60,125 @@ export class RequestError extends Error {
 
 export type Client = (verb: Verb, path: string, body?: string) => Promise<any>;
 
-export const CertificateFingerprintSet = [
-  "20:96:86:F8:FE:11:4A:17:23:99:99:FF:EC:4C:14:BA:2A:89:55:3E:9E:5A:3C:78:19:A6:63:C9:13:B7:93:7D",
-];
+export const PublicKeyPinSet = ["gg3x7U4UrWfTUpYNy9wL2+GYOQhi3fg5UTn5pzA67gc="];
 
-const CACertificateBundle = readFileSync(__dirname + "/../data/cacert.pem");
+const defaultOptions: Omit<Options, "projectId"> = {
+  host: "api.eventline.net",
+  port: 443,
+  scheme: "https",
+};
 
 export function makeClient(opts: Options): Client {
-  const host: string = opts.host ?? "api.eventline.net";
-  const port: number = opts.port ?? 443;
-  const scheme: string = opts.scheme ?? "https";
-  const token: string = opts.token ?? process.env["EVCLI_API_KEY"] ?? "";
-  const endpoint: URL = new URL(`${scheme}://${host}:${port}`);
+  const options = { ...defaultOptions, ...opts };
+  const timeout = 30_000;
+  const ca = fs.readFileSync(__dirname + "/../data/cacert.pem");
+  const accessToken = opts.token ?? process.env["EVCLI_API_KEY"];
 
-  const defaultHeaderFields: Record<string, string> = {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-    "User-Agent": "eventline-sdk-js",
-    "X-Eventline-Project-Id": opts.projectId,
-    Authorization: "Bearer " + token,
-  };
+  const baseURL = url.format({
+    protocol: options.scheme,
+    host: options.host,
+    port: options.port,
+  });
 
-  return function (
-    verb: Verb,
-    path: string,
-    body?: string
-  ): Promise<object | string> {
-    const url = new URL(path, endpoint);
-    let headerFields = defaultHeaderFields;
+  return function (method: Verb, path: string, body?: string): Promise<any> {
+    const httpAgentOptions = {
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + accessToken,
+        "Content-Type": "application/json",
+        "Content-Length": (body ?? "").length,
+        "User-Agent": "eventline-sdk-js",
+        "X-Eventline-Project-Id": options.projectId,
+      },
+      method,
+      timeout,
+      ca,
+      checkServerIdentity: function (host: string, cert: PeerCertificate) {
+        const err = tls.checkServerIdentity(host, cert);
+        if (err) return err;
 
-    if (body !== undefined)
-      headerFields = {
-        "Content-Length": body.length.toString(),
-        ...defaultHeaderFields,
-      };
+        const fingerprint = crypto
+          .createHash("sha256")
+          .update((cert as any).pubkey)
+          .digest("base64");
 
-    const options = {
-      method: verb,
-      headers: headerFields,
-      timeout: 30_000,
-      ca: CACertificateBundle,
+        if (PublicKeyPinSet.indexOf(fingerprint) === -1) {
+          const msg =
+            "Certificate verification error: " +
+            `The public key of '${cert.subject.CN}' ` +
+            "does not match any pinned fingerprints";
+          return new Error(msg);
+        }
+        return;
+      },
+    };
+
+    const httpOptions = {
+      ...httpAgentOptions,
+      agent: new https.Agent(httpAgentOptions),
     };
 
     return new Promise(function (resolve, reject) {
-      const request = httpRequest(url, options, function (response) {
-        let buf = "";
+      const request = https.request(
+        new URL(path, baseURL),
+        httpOptions,
+        function (response) {
+          let buf = "";
 
-        response.resume();
-        response.on("data", (chunk) => (buf += chunk));
-        response.on("end", () => {
-          let hasSucceeded = true;
-          const status = response.statusCode;
-          const responseType = response.headers["content-type"];
+          response.resume();
+          response.on("data", (chunk) => (buf += chunk));
+          response.on("end", () => {
+            let hasSucceeded = true;
+            const status = response.statusCode;
+            const responseType = response.headers["content-type"];
 
-          if (!response.complete)
-            return reject(
-              new RequestError(
-                status,
-                "incomplete_response",
-                {},
-                "incomplete response"
-              )
-            );
+            if (!response.complete)
+              return reject(
+                new RequestError(
+                  status,
+                  "incomplete_response",
+                  {},
+                  "incomplete response"
+                )
+              );
 
-          if (status === undefined || status < 200 || status >= 300)
-            hasSucceeded = false;
+            if (status === undefined || status < 200 || status >= 300)
+              hasSucceeded = false;
 
-          if (responseType === "application/json") {
-            try {
-              const data = JSON.parse(buf);
+            if (responseType === "application/json") {
+              try {
+                const data = JSON.parse(buf);
 
-              if (hasSucceeded) resolve(data);
-              else
+                if (hasSucceeded) resolve(data);
+                else
+                  reject(
+                    new RequestError(
+                      status,
+                      data.code ?? "unknown_error",
+                      data.data ?? {},
+                      data.error
+                    )
+                  );
+              } catch {
                 reject(
                   new RequestError(
                     status,
-                    data.code ?? "unknown_error",
-                    data.data ?? {},
-                    data.error
+                    "invalid_json",
+                    buf,
+                    "invalid json body"
                   )
                 );
-            } catch {
-              reject(
-                new RequestError(
-                  status,
-                  "invalid_json",
-                  buf,
-                  "invalid json body"
-                )
-              );
+              }
+            } else {
+              if (hasSucceeded) resolve(buf);
+              else reject(new RequestError(status, "unknown_error", buf));
             }
-          } else {
-            if (hasSucceeded) resolve(buf);
-            else reject(new RequestError(status, "unknown_error", buf));
-          }
-        });
-      });
+          });
+        }
+      );
       request.on("error", reject);
       request.on("timeout", request.abort);
-      request.on("socket", function (socket: TLSSocket) {
-        socket.on("secureConnect", function () {
-          const fingerprint = socket.getPeerCertificate().fingerprint256;
-
-          if (socket.authorized === false) {
-            request.emit(
-              "error",
-              new Error(socket.authorizationError.toString())
-            );
-            request.abort();
-            return;
-          }
-
-          if (
-            CertificateFingerprintSet.indexOf(fingerprint) === -1 &&
-            !socket.isSessionReused()
-          ) {
-            request.emit(
-              "error",
-              new Error("Certificate fingerprint does not match")
-            );
-            request.abort();
-            return;
-          }
-        });
-      });
-      if (body !== undefined) request.write(body);
+      request.write(body ?? "");
       request.end();
     });
   };
