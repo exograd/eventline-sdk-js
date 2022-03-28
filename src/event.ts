a3fef079f8be44a1c53946dcd51d2b317929ec1c
gearnode <bryan.frimin@exograd.com>
2022-03-28 13:23:31 +0200

    Update event documentation

--- src/event.ts
+++ src/event.ts
@@ -21,14 +21,55 @@ import url from "url";
 import { buildPaginationQuery } from "./client";
 
 export interface Event {
+  /**
+   * The identifier of the event.
+   */
   id: Id;
+
+  /**
+   * The identifier of the organization the event is part of.
+   */
   org_id: Id;
+
+  /**
+   * The identifier of the trigger at the origin of the event
+   * (optional). Only present if the event was created following the
+   * activation of a trigger.
+   */
   trigger_id?: Id;
+
+  /**
+   * The identifier of the command at the origin of the event
+   * (optional). Only present if the event was created following the
+   * execution of a command.
+   */
   command_id?: Id;
+
+  /**
+   * The date the event was created.
+   */
   creation_time: string;
+
+  /**
+   * The date the event actually happened.
+   */
   event_time: string;
+
+  /**
+   * The name of the connector.
+   */
   name: string;
+
+  /**
+   * The name of the event.
+   */
   data: Object;
+
+  /**
+   * The set of data contained by the event. Depends on the connector
+   * and name. See the documentation of the event on the connector page
+   * for more information.
+   */
   original_event_id?: Id;
 }
 
@@ -40,6 +81,30 @@ export interface ListEventsRequest extends Pagination {
 
 export type ListEventsResponse = ListResponse<Event>;
 
+export interface GetEventRequest {
+  id: Id;
+}
+
+export type GetEventResponse = Event;
+
+export interface CreateEventRequest {
+  eventTime?: Date | string;
+  connector: string;
+  name: string;
+  data: object;
+}
+
+export type CreateEventResponse = Event;
+
+export interface ReplayEventRequest {
+  id: Id;
+}
+
+export type ReplayEventResponse = Event;
+
+/**
+ * Fetch a list of events.
+ */
 export async function listEvents(
   client: Client,
   request: ListEventsRequest
@@ -54,12 +119,9 @@ export async function listEvents(
   return client("GET", url.format({ pathname: "/v0/events", query: q }));
 }
 
-export interface GetEventRequest {
-  id: Id;
-}
-
-export type GetEventResponse = Event;
-
+/**
+ * Fetch an event by identifier.
+ */
 export async function getEvent(
   client: Client,
   request: GetEventRequest
@@ -67,15 +129,9 @@ export async function getEvent(
   return client("GET", "/v0/events/id/" + request.id);
 }
 
-export interface CreateEventRequest {
-  eventTime?: Date | string;
-  connector: string;
-  name: string;
-  data: object;
-}
-
-export type CreateEventResponse = Event;
-
+/**
+ * Create a new custom event.
+ */
 export async function createEvent(
   client: Client,
   request: CreateEventRequest
@@ -90,12 +146,9 @@ export async function createEvent(
   return client("POST", "/v0/events", data);
 }
 
-export interface ReplayEventRequest {
-  id: Id;
-}
-
-export type ReplayEventResponse = Event;
-
+/**
+ * Create a new custom event.
+ */
 export async function replayEvent(
   client: Client,
   request: ReplayEventRequest
