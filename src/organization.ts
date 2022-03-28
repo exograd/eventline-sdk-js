b62de1efccf4adfe23d3664305f7256bb3a8b123
gearnode <bryan.frimin@exograd.com>
2022-03-28 13:11:31 +0200

    Update organization documentation

--- src/organization.ts
+++ src/organization.ts
@@ -18,15 +18,51 @@ import type { Id } from "@ev";
 import type { Client } from "@ev/client";
 
 export interface Organization {
+  /**
+   * The identifier of the organization.
+   */
   id: Id;
+
+  /**
+   * The name of the organization.
+   */
   name: string;
+
+  /**
+   * The registered address of the organization.
+   */
   address: string;
+
+  /**
+   * The postal code of the registered address of the organization.
+   */
   postal_code: string;
+
+  /**
+   * The city of the registered address of the organization.
+   */
   city: string;
+
+  /**
+   * The country of the registered address of the organization.
+   */
   country: string;
-  disabled: boolean;
+
+  /**
+   * The email address used as primary contact by the organization.
+   */
   contact_email_address: string;
+
+  /**
+   * A flag indicating whether the organization accepts to receive
+   * non-essential emails from Exograd or not (optional, default is
+   * false). See our privacy policy for more information.
+   */
   non_essential_mail_opt_in: boolean;
+
+  /**
+   * The VAT identification number (optional).
+   */
   vat_id_number?: string;
 }
 
@@ -34,6 +70,10 @@ export interface GetOrganizationRequest {}
 
 export type GetOrganizationResponse = Organization;
 
+/**
+ * Fetch the organization associated with the credentials currently used
+ * by the client.
+ */
 export async function getOrganization(
   client: Client,
   _request: GetOrganizationRequest
